5024df1e3bdc8c95dbb532a5af97b68a62a88f46
Alina Malyutina <linamalutina1@gmail.com>
2022-11-07 21:51:05 +0300

    add 2 task - count product of numbers between 1 and entered number

---
+++ N!/Program.cs
@@ -0,0 +1,19 @@
+﻿int ProdFromTo(int arg1)
+{
+    int prod = 1;
+    
+    for(int i = 1; i<=Math.Abs(arg1); i++)
+    {
+        prod = prod * i;
+    }
+    return prod; 
+}
+
+Console.WriteLine("This program assepts number and show product of numbers between 1 and your number");
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int product = ProdFromTo(number);
+Console.WriteLine(product);
+
+
