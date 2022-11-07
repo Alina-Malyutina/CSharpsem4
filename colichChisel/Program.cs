e8e67f08a2e6d132b58b6d95f81e125f5261a039
Alina Malyutina <linamalutina1@gmail.com>
2022-11-07 21:50:02 +0300

    add 2 task - count product of numbers between 1 and entered number

---
+++ colichChisel/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("This program count quantity of digits in number.");
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string strNumber = Math.Abs(number).ToString();
+Console.WriteLine(strNumber.Length);
+
