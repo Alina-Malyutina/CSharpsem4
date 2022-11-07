9343833e2aa5d954a804453646e935d3d337b2d9
Alina Malyutina <linamalutina1@gmail.com>
2022-11-07 21:47:37 +0300

    add 1 task - count sum of numbers between 1 and entered number

---
+++ sum1toA/Program.cs
@@ -0,0 +1,25 @@
+﻿int SumFromTo(int arg1)
+{
+    int summ = 0;
+
+    /*int temp = 1;
+     while (temp < arg1)
+    {
+        summ = summ + temp;
+        temp++;
+    }*/
+    
+    for(int i = 1; i<=arg1; i++)
+    {
+        summ = summ + i;
+    }
+    return summ; 
+}
+
+Console.WriteLine("This program assepts number and show sum numbers between 1 and number");
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumFromTo(number);
+Console.WriteLine(sum);
+
